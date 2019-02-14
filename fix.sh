3e1291cabed6d87db489430f637435f133ca7574
Colin Maudry <colin@maudry.com>
2019-02-14 19:51:24 +0100

    Script racine fix.sh pour la correction d'anomalies

---
+++ fix.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#**********************************************************************
+#
+# Correction des anomalies dans les données source
+#
+#**********************************************************************
+
+# Pour facilement s'y retrouver dans les répertoires, création d'une variable avec le chemin de la racine de decp-json
+export DECP_HOME=`pwd`
+source=$1
+
+if [[ -d $DECP_HOME/sources/$source ]]
+    then
+
+    cd $DECP_HOME/sources/$source
+
+    if [[ ! -d original-data ]]
+    then
+        mkdir original-data
+        cp *.* original-data/
+    fi
+
+    rm *.*
+
+    $DECP_HOME/scripts/sources/$source/fix.sh $source
+
+elif [[ -z "$source" ]]
+    then
+    echo "Récupération de toutes les sources (désactivé)"
+else
+    cd ./json
+    echo "Cette source n'existe pas"
+    echo ""
+    echo "Voici les sources supportées pour GET :"
+    echo ""
+    ls -d1 */
+    echo ""
+
+    exit 1
+fi
