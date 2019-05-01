82b9d00dcd37e733909c1b1f6269072e032d2199
Colin Maudry <colin@maudry.com>
2019-05-01 20:36:27 +0200

    Simplification de la gestion des variables, et distinction API key prod et API key next

--- publish.sh
+++ publish.sh
@@ -11,35 +11,29 @@
 
 case ${CIRCLE_BRANCH} in
     master)
-    export API_URL="https://data.gouv.fr/api/1"
-    export DATASET_ID="?"
-    export DECP_JSON_RESOURCE_ID="?"
+    export api="https://data.gouv.fr/api/1"
+    export dataset_id="?"
+    export resource_id="?"
+
+    #API_KEY configurée dans les options de build de CircleCI
+    api_key=$API_KEY
     ;;
 
     *)
-    export API_URL="https://next.data.gouv.fr/api/1"
-    export DATASET_ID="5cc1bb51dc470946203cc376"
-    export DECP_JSON_RESOURCE_ID="5f550643-9867-41ee-a77c-8d7739383dbe"
+    export api="https://next.data.gouv.fr/api/1"
+    export dataset_id="5cc1bb51dc470946203cc376"
+    export resource_id="5f550643-9867-41ee-a77c-8d7739383dbe"
+    api_key=$NEXT_API_KEY
     ;;
 esac
 
-
-
-
 if [[ ! -f ./json/decp.json ]]
 then
-    echo "Le fichier agrégé ./json/decp.json doit d'abord être généré."
+    echo "Le fichier agrégé ./json/decp.json doit d'abord être généré avec la commande './merge_all_sources.sh'."
     exit 1
 fi
 
-api=$API_URL
-
-#API_KEY configurée dans les options de build de CircleCI
-api_key=$API_KEY
-dataset_id=$DATASET_ID
-resource_id=$DECP_JSON_RESOURCE_ID
-
-# Remplacement du decp.json par sa mise à jour quotidienne
+echo "Remplacement du decp.json par sa mise à jour quotidienne"
 curl $api/datasets/$dataset_id/resources/$resource_id/upload/ -F "file=@json/decp.json" -H "X-API-KEY: $api_key" | jq .success
 
 # Si nous sommes le premier du mois, publication d'une nouvelle archive mensuelle
@@ -47,8 +41,8 @@ if [[ `date +%d` -eq "01"  ]]
 then
     date=`date +%d/%m/%Y`
 
-    # Upload du fichier comme nouvelle ressource
-    curl $api/datasets/$dataset_id/upload/ -F "file=@json/decp.json" -F "filename=decp-$date" -H "X-API-KEY: $api_key" > new_resource.json
+    echo "Upload du fichier comme nouvelle ressource"
+    curl -v $api/datasets/$dataset_id/upload/ -F "file=@json/decp.json" -F "filename=decp-$date" -H "X-API-KEY: $api_key" > new_resource.json
 
     echo "new_resource.json"
     jq . new_resource.json
@@ -60,7 +54,7 @@ then
     echo "new_resource_modified.json"
     jq . new_resource_modified.json
 
-    # Modification du titre de la nouvelle ressource
+    echo "Modification du titre de la nouvelle ressource"
     curl -v -X PUT $api/datasets/$dataset_id/resources/$new_resource_id/ --data-binary "@new_resource_modified.json" -H "Content-type: application/json" -H "X-API-KEY: $api_key"
 
     rm new_resource.json
