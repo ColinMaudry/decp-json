cabef627bee2be3f43c0a777bec05fff1af1fb66
Colin Maudry <colin@maudry.com>
2019-05-22 15:32:43 +0200

    Ajout de logs

--- scripts/jsonDECP2xmlDECP.sh
+++ scripts/jsonDECP2xmlDECP.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Ce script convertit les données DECP JSON en DECP XML
+echo ""
+echo "Conversion de $1 vers DECP XML..."
 
 json2xml.py $1 | xsltproc scripts/xslt/postJsonConversion.xsl - | xmllint --format --encode utf-8 -
