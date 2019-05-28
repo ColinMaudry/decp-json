4dbc340b49471d98459b8df962136971dffe3436
Colin Maudry <colin@maudry.com>
2019-05-28 15:23:28 -0400

    Meilleure URL data.gouv.fr (pas de redir)

--- scripts/makeOCDS.sh
+++ scripts/makeOCDS.sh
@@ -8,7 +8,7 @@
 
 datetime=`date "+%FT%T"`
 dataset_id="5cc1bb51dc470946203cc376"
-dataset_url="https://data.gouv.fr/fr/datasets/$dataset_id"
+dataset_url="https://www.data.gouv.fr/fr/datasets/$dataset_id"
 ocid_prefix="ocds-78apv2"
 
-jq --arg datetime $datetime --arg datasetUrl $dataset_url --arg ocidPrefix $ocid_prefix -f jq/decp2ocds.jq $1
+jq --arg datetime $datetime --arg datasetUrl $dataset_url --arg ocidPrefix $ocid_prefix -f scripts/jq/decp2ocds.jq $1
