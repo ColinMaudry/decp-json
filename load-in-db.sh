3c265ec0d48b1460ee109ff8594d34a04f65816d
Colin Maudry <colin@maudry.com>
2019-06-05 08:47:31 +0200

    Ajustement du load

--- load-in-db.sh
+++ load-in-db.sh
@@ -8,8 +8,13 @@
 
 source config/config.sh
 
-wget https://www.data.gouv.fr/fr/datasets/r/16962018-5c31-4296-9454-5998585496d2 -o json/decp.json
-
 
 # Chargement des métadonnées des sources
-mongoimport --username $mongoUsername --password $mongoPassword --host $mongoHost:$mongoPort --db $mongoDatabase --collection data --drop --jsonArray json/decp.json
+wget https://raw.githubusercontent.com/etalab/decp-rama/master/sources/metadata.json -O sources/metadata.json
+
+mongoimport --username $mongoUsername --password $mongoPassword --host $mongoHost:$mongoPort --db $mongoDatabase --collection sources --drop --jsonArray --file sources/metadata.json
+
+# Chargement des données agrégées de decp-rama
+wget https://www.data.gouv.fr/fr/datasets/r/16962018-5c31-4296-9454-5998585496d2 -O json/decp.json
+
+jq '.marches' json/decp.json | mongoimport --username $mongoUsername --password $mongoPassword --host $mongoHost:$mongoPort --db $mongoDatabase --collection data --drop --jsonArray
