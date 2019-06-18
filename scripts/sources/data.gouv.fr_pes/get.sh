6029081b7faa355b3fb2c8e5083674ef946f978a
Colin Maudry <colin@maudry.com>
2019-06-18 23:09:44 +0200

    Téléchargement moins verbeux pour les données du PES

--- scripts/sources/data.gouv.fr_pes/get.sh
+++ scripts/sources/data.gouv.fr_pes/get.sh
@@ -7,7 +7,7 @@
 
 # Récupération des données, tous les fichiers sont stockés dans le même dossier
 # Le nombre de fichiers sur cette source pouvant un jour atteindre des sommets, cette solution n'est pas pérenne
-wget -nv -r -nd -A xml http://files.data.gouv.fr/decp/
+wget -q -r -nd -A xml http://files.data.gouv.fr/decp/
 
 count=`ls -1 | wc -l`
 
