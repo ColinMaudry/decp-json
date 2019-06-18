1985f5de400fe31e012bc1f2534b0906f8500cdc
Colin Maudry <colin@maudry.com>
2019-06-19 00:04:15 +0200

    Rétablissement des logs pour maintenir le run Circle en vie
    Erreur
    SIGHUP received,redirecting output to ‘wget-log’. Too long with no output (exceeded 10m0s)

--- scripts/sources/data.gouv.fr_pes/get.sh
+++ scripts/sources/data.gouv.fr_pes/get.sh
@@ -7,7 +7,10 @@
 
 # Récupération des données, tous les fichiers sont stockés dans le même dossier
 # Le nombre de fichiers sur cette source pouvant un jour atteindre des sommets, cette solution n'est pas pérenne
-wget -q -r -nd -A xml http://files.data.gouv.fr/decp/
+
+# Un minimum de log est nécessaire pour maintenir le run Circle, autrement :
+# "SIGHUP received,redirecting output to ‘wget-log’. Too long with no output (exceeded 10m0s)"
+wget -nv -r -nd -A xml http://files.data.gouv.fr/decp/
 
 count=`ls -1 | wc -l`
 
