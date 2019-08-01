b33142b6a5ae940c60d7e20b3ed8fb92a98afb98
Colin Maudry <colin@maudry.com>
2019-08-01 11:07:01 +0200

    Added flattened (xlsx) output to makeOCDS.sh #1

--- scripts/makeOCDS.sh
+++ scripts/makeOCDS.sh
@@ -19,9 +19,20 @@ dataset_url="https://www.data.gouv.fr/fr/datasets/$dataset_id"
 ocid_prefix="ocds-78apv2"
 
 # Création du fichier de release OCDS à partir des DECP
+jq --arg datetime $datetime --arg datasetUrl $dataset_url --arg ocidPrefix $ocid_prefix --arg packageUri $package_uri -f scripts/jq/ocds/decp2ocds.jq $1 > temp
 
-jq --arg datetime $datetime --arg datasetUrl $dataset_url --arg ocidPrefix $ocid_prefix --arg packageUri $package_uri -f scripts/jq/ocds/decp2ocds.jq $1 > json/releases.json
+
+if [[ $2 -eq "allFormats" ]]
+then
+
+mv temp json/releases.json
 
 # Création des fichiers tabulaires à partir des releases JSON
 
 flatten-tool flatten json/releases.json --root-id=ocid --main-sheet-name releases --root-list-path=releases
+mv flattened.xlsx json/
+
+else
+    cat temp
+    rm temp
+fi
