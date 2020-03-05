17fde77abcac5b48fe0824cc50cc8ccc5733f8e1
Colin Maudry <colin@maudry.com>
2020-03-05 11:21:28 +0100

    Suppression des scripts et des données sources

--- scripts/jq/addLastDownloadTime.jq
+++
@@ -1,2 +0,0 @@
-
-.[] |= if .code == $source then .lastDownload |= (now | todate) else . end
--- scripts/xmlDECP2jsonDECP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Ce script convertit les données en DECP JSON
-
-xml2json $1 | jq -f $DECP_HOME/scripts/jq/jsonDECP.jq
