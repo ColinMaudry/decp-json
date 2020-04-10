8d685d3d08900342f425f5d27241e9b5fe1ff467
Colin Maudry <colin.maudry@data.gouv.fr>
2020-04-10 18:47:52 +0200

    Disabling the creating of flattened files for now #1

--- scripts/makeOCDS.sh
+++ scripts/makeOCDS.sh
@@ -14,13 +14,13 @@ source $DECP_HOME/scripts/jq/ocds/config.sh
 
 $DECP_HOME/scripts/makeOCDS_json.sh $1 > $DECP_HOME/json/releases.ocds.json
 
-if [[ $2 -eq "allFormats" ]]
-then
-
-# Création des fichiers tabulaires à partir des releases JSON
-
-flatten-tool flatten json/releases.ocds.json --root-id=ocid --main-sheet-name releases --root-list-path=releases
-
-mv flattened.xlsx flattened/
-
-fi
+# if [[ $2 -eq "allFormats" ]]
+# then
+#
+# # Création des fichiers tabulaires à partir des releases JSON
+#
+# flatten-tool flatten json/releases.ocds.json --root-id=ocid --main-sheet-name releases --root-list-path=releases
+#
+# mv flattened.xlsx flattened/
+#
+# fi
