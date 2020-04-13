a35d5fdf3fa584b7e141a2eebc78d02ada8c3814
Colin Maudry <colin.maudry@data.gouv.fr>
2020-04-13 11:23:32 +0200

    Added config to makeOCDS.sh #1

--- scripts/makeOCDS.sh
+++ scripts/makeOCDS.sh
@@ -9,6 +9,17 @@
 # fail on error
 set -e
 
+if [[ -z $datetime ]]
+then
+ export datetime=`date "+%FT%T+02:00"`
+fi
+
+export dataset_id="5d1a216e6f4441513e89b93e"
+export package_uri="https://www.data.gouv.fr/fr/datasets/r/2a84ffc7-7121-48d2-a28f-85d835ed09a4"
+export dataset_url="https://www.data.gouv.fr/fr/datasets/$dataset_id"
+export ocid_prefix="ocds-78apv2"
+export DECP_HOME=`pwd`
+
 # Récupération des variables injectées dans l'OCDS
 source $DECP_HOME/scripts/jq/ocds/config.sh
 
