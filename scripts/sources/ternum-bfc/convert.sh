f9d79c49dd4d16ea5520938830ac5110388699cc
Colin Maudry <colin@maudry.com>
2020-10-01 16:04:17 +0200

    Ajout des données Ternum BFC (#45)
    * Script de télécargement des fichiers ternum BFC
    
    * Ajout de la source ternum-bfc, correction de la description de la source atexo-maximilien
    
    * Script de conversion partagé

---
+++ scripts/sources/ternum-bfc/convert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+source=$1
+
+$DECP_HOME/scripts/sources/shared/convert-xml.sh $source
---
+++ scripts/sources/ternum-bfc/get.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+# Récupération des données
+
+
+wget -r -q -nc -nd -nH --accept=xml -np  http://focus-marches.ternum-bfc.fr/xml/
